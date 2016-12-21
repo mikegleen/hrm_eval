bfa76039c013e3e1028ca33c536f30fb63a83123
Mike Gleen <mike.gleen@gmail.com>
2016-12-21 17:57:54 +0100

    Initial commit.

---
+++ assign_nums.py
@@ -0,0 +1,40 @@
+"""
+    Assign question numbers. In the input file, questions start at column 10
+    (column 9 if zero based). Only non-empty cells are counted.
+"""
+
+import csv
+import sys
+
+SKIPCOLS = 9
+
+
+def assign_numbers(row):
+    nums = ['' for n in range(SKIPCOLS)]
+    q_num = 0
+    for cell in row[SKIPCOLS:]:
+        if cell:
+            q_num += 1
+            nums.append(q_num)
+        else:
+            nums.append('')
+    return nums
+
+
+def main():
+    infile = open(sys.argv[1], newline='')
+    reader = csv.reader(infile)
+    outfile = open(sys.argv[2], 'w', newline='')
+    writer = csv.writer(outfile)
+    row = next(reader)
+    nums = assign_numbers(row)
+    writer.writerow(nums)
+    writer.writerow(row)
+    for row in reader:
+        writer.writerow(row)
+
+
+if __name__ == '__main__':
+    if sys.version_info.major < 3:
+        raise ImportError('requires Python 3')
+    main()
