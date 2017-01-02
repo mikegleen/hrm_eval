a038ed3ed9513598b53ade628b3ebe3a90d6fd5e
Mike Gleen <mike.gleen@gmail.com>
2017-01-02 11:33:23 +0100

    Initial commit.

---
+++ src/postcode.py
@@ -0,0 +1,57 @@
+import haversine
+import json
+import requests
+import sys
+import time
+
+WHR_LOCATION = (51.5920, -.3870)  # lat, lng
+
+URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+payload = {'sensor': 'false'}
+
+
+def get_address(postcode):
+    payload['address'] = ''.join(postcode.split())  # remove all whitespace
+    response = requests.get(URL, params=payload)
+    return response.json()
+
+
+def get_distance(postcode):
+    print('trying: ', postcode)
+    j = get_address(postcode)
+    # print(json.dumps(j, indent=4))
+    l = j['results'][0]['geometry']['location']
+    addr = j['results'][0]['formatted_address']
+    location = (l['lat'], l['lng'])
+    # print(location)
+    distance = haversine.haversine(WHR_LOCATION, location, miles=True)
+    return distance, addr
+
+
+def main(postcodefilename, addressfilename):
+    postcodefile = open(postcodefilename)
+    addressfile = open(addressfilename, 'w')
+
+    for postcode in postcodefile:
+        time.sleep(2)
+        postcode = ''.join(postcode.split())
+        if postcode:
+            distance, addr = get_distance(postcode)
+            print('{:9},{:8.2f}, {}'.format(postcode, distance, addr),
+                  file=addressfile)
+
+
+def one_postcode(postcode):
+    postcode = ''.join(postcode.split())
+    distance, addr = get_distance(postcode)
+    return postcode, distance, addr
+
+if __name__ == '__main__':
+    if sys.version_info.major < 3:
+        raise ImportError('requires Python 3')
+    if len(sys.argv) > 2:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        row = one_postcode(sys.argv[1])
+        print('{},{:8.2f}, {}'.format(*row))
+
