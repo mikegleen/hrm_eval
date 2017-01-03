89c72c269f473e431933da095e77720776608239
Mike Gleen <mike.gleen@gmail.com>
2017-01-03 10:31:09 +0100

    Add JSON save/restore.

--- src/postcode.py
+++ src/postcode.py
@@ -1,18 +1,79 @@
+"""
+Update our database of postcodes, retaining the distance from the museum.
+The database is stored in a JSON file in DBPATH with a name based on the
+creation date and time.
+
+Input is a text file with one line per postcode; empty lines are ignored. You
+may have to delete the top row(s) when extracting the data from a spreadsheet.
+
+If the postcode from the text file is not in the database, the information is
+fetched from google maps.
+"""
+from collections import namedtuple
+import datetime
 import haversine
 import json
+import os.path
 import requests
 import sys
 import time
 
 WHR_LOCATION = (51.5920, -.3870)  # lat, lng
 URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+PcData = namedtuple('PcData', 'distance address'.split())
+
+"""
+    The DBPATH directory contains the latest JSON files with the most recently
+    created table of postcode data.
+"""
+DBPATH = os.path.join('data', 'postcode')
 
 
-def get_address(postcode):
+def load_json():
+    """
+    Iterate over the JSON files in DBPATH, find the most recent one.
+    :return: The loaded dictionary.
+    """
+    files = os.listdir(DBPATH)
+    pcdict = {}
+    jsonfilename = None
+    for f in sorted(files, reverse=True):
+        print('-->', f)
+        if f.endswith('.json'):
+            jsonfilename = os.path.join(DBPATH, f)
+            break
+    try:
+        with open(jsonfilename) as jsonfile:
+            print('Loading database file {}'.format(jsonfilename))
+            pcdict = json.load(jsonfile)
+            print('{} postcodes loaded'.format(len(pcdict)))
+    except TypeError:  # if no files found
+        print('No files found in {}'.format(DBPATH))
+    return pcdict
+
+
+def save_json(pcdict):
+    filename = datetime.datetime.today().strftime("%Y%m%d-%H%M%S.json")
+    filepath = os.path.join(DBPATH, filename)
+    with open(filepath, 'w') as jsonfile:
+        json.dump(pcdict, jsonfile, indent=4)
+    print('{} postcodes saved to {}.'.format(len(pcdict), filename))
+
+
+def get_json(postcode):
+    """
+    :param postcode: UK Postcode with all white space removed.
+    :return: The dictionary of the JSON string returned by requests.
+    """
     payload = dict(sensor='false')
-    address = ''.join(postcode.split())  # remove all whitespace
-    payload['address'] = address + ',UK'
+    payload['address'] = postcode + ',UK'
     response = requests.get(URL, params=payload)
+    if response.status_code != 200:
+        print('Retrying ' + postcode)
+        time.sleep(5)
+        response = requests.get(URL, params=payload)
+        if response.status_code != 200:
+            raise IOError('status {} returned'.format(response.status_code))
     return response.json()
 
 
@@ -23,7 +84,7 @@ def get_distance(postcode):
              formatted address as a string.
     """
     print('trying: ', postcode)
-    j = get_address(postcode)
+    j = get_json(postcode)
     # print(json.dumps(j, indent=4))
     l = j['results'][0]['geometry']['location']
     addr = j['results'][0]['formatted_address']
@@ -36,18 +97,25 @@ def get_distance(postcode):
 def main(postcodefilename, addressfilename):
     postcodefile = open(postcodefilename)
     addressfile = open(addressfilename, 'w')
-
+    pcdata = load_json()
+    save_pcdata_len = len(pcdata)
     for postcode in postcodefile:
-        time.sleep(2)  # google gets annoyed if we hit it too quickly.
-        postcode = ''.join(postcode.split())
-        if postcode:
+        time.sleep(1)  # google gets annoyed if we hit it too quickly.
+        postcode = ''.join(postcode.split()).upper()
+        if postcode and postcode not in pcdata:
             distance, addr = get_distance(postcode)
-            print('{:9},{:8.2f}, {}'.format(postcode, distance, addr),
-                  file=addressfile)
+            pcdata[postcode] = PcData(distance, addr)
+    for postcode in sorted(pcdata):
+        distance = pcdata[postcode][0]
+        addr = pcdata[postcode][1]
+        print('{:9},{:8.2f}, {}'.format(postcode, distance, addr),
+              file=addressfile)
+    if len(pcdata) > save_pcdata_len:
+        save_json(pcdata)
 
 
 def one_postcode(postcode):
-    postcode = ''.join(postcode.split())
+    postcode = ''.join(postcode.split()).upper()
     distance, addr = get_distance(postcode)
     return postcode, distance, addr
 
