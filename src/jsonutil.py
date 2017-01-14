eedecf79fd963d7b6ceea0dc8d5c3ed277b9f0dd
Mike Gleen <mike.gleen@gmail.com>
2017-01-14 18:01:04 +0000

    Move json load/save to another file, jsonutil.py.

---
+++ src/jsonutil.py
@@ -0,0 +1,53 @@
+import datetime
+import json
+import os
+
+MAXJSONFILES = 5  # The number of JSON files to save. Older ones are deleted.
+
+
+def load_json(dbpath):
+    """
+    Iterate over the JSON files in DBPATH, find the most recent one.
+    :return: The loaded dictionary.
+    """
+    files = os.listdir(dbpath)
+    pcdict = {}
+    jsonfilename = None
+    for f in sorted(files, reverse=True):
+        print('-->', f)
+        if f.endswith('.json'):
+            jsonfilename = os.path.join(dbpath, f)
+            break
+    try:
+        with open(jsonfilename) as jsonfile:
+            print('Loading database file {}'.format(jsonfilename))
+            pcdict = json.load(jsonfile)
+            print('{} records loaded'.format(len(pcdict)))
+    except TypeError:  # if no files found
+        print('No files found in {}'.format(dbpath))
+    return pcdict
+
+
+def save_json(pcdict, dbpath):
+    """
+    Create a JSON file in the DBPATH directory and delete any old JSON files
+    found.
+
+    :param pcdict: the dictionary containing the postcode data.
+    :param dbpath: the path to the directory to save the JSON file in.
+    :return: None
+    """
+    os.makedirs(dbpath, exist_ok=True)
+    filename = datetime.datetime.today().strftime("%Y%m%d-%H%M%S.json")
+    filepath = os.path.join(dbpath, filename)
+    with open(filepath, 'w') as jsonfile:
+        json.dump(pcdict, jsonfile, indent=4)
+    print('{} records saved to {}.'.format(len(pcdict), filename))
+    # delete old json files
+    files = sorted(os.listdir(dbpath))
+    files = [f for f in files if f.endswith('.json')]
+    if len(files) <= MAXJSONFILES:
+        return
+    for f in files[:len(files) - MAXJSONFILES]:
+        os.remove(os.path.join(dbpath, f))
+        print('Deleting ', os.path.join(dbpath, f))
