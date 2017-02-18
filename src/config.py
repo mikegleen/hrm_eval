250dab37b4efc5750814439061846a8fe5eb4f51
Mike Gleen <mike.gleen@gmail.com>
2017-02-18 17:53:40 -0600

    Update definitions of CSV columns.

--- src/config.py
+++ src/config.py
@@ -2,9 +2,11 @@
 Configuration file for processing SurveyMonkey CSV files.
 
 This configuration assumes that the CSV file has been cleaned up by
-get_response.sh which adds a row at the top containing the question numbers.
+get_response.sh which adds a row at the top containing the question numbers and
+merges multiple CSV files into a single spreadsheet.
 
-To use the unmodified export from SurveyMonkey, change skiprows to 2.
+To use the unmodified export from SurveyMonkey, change skiprows to 2 and adjust
+the column numbers accordingly.
 
 """
 from excel_cols import col2num
@@ -24,5 +26,13 @@ DEFCOL = {
     'want_notify': 'JT',
     'volunteer': 'JU',
     'email_addr': 'JY',
+}
+# updated 2017-02-18:
+DEFCOL = {
+    'postcode': 'IJ',
+    'want_newsletter': 'KE',
+    'want_notify': 'KF',
+    'volunteer': 'KG',
+    'email_addr': 'KK',
 }
 defcol = {dc: col2num(DEFCOL[dc]) for dc in DEFCOL}
