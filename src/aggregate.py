9cc08da89966c62c191019a272402216942425f8
Mike Gleen <mike.gleen@gmail.com>
2018-03-21 14:53:56 +0000

    Add validation code to make sure that the config table AGGLIST matches the data in row 3 of the spreadsheet.

--- src/aggregate.py
+++ src/aggregate.py
@@ -8,14 +8,15 @@ to 'under 55' and '55 and over'.
 Input is the cleaned file produced by extract_csv.sh.
 
 Processing depends upon the three header rows:
-1. Text q[minor] where minor is the question number in the first column corresponding
-   to the Nth question
-2. In the column where a q[minor] appears in row 1, row 2 contains the name of the
-   question.
-3. Each column associated with a question contains a possible answer.
+Row 1. Text q[n] where n is the question number in the first column
+   corresponding to the Nth question
+Row 2. In the column where a q[n] appears in row 1, row 2 contains the name of
+   the question.
+Row 3. Each column associated with a question contains a possible answer.
 
 See crosstabs.py for how to create the input CSV file.
 
+TODO:
 In AGGLIST, if an 'oldcols' entry has a leading asterisk, there will be no
 existing column of that name but it should be created, meaning that it will
 count when no responses are selected. This must be the last column defined.
@@ -28,28 +29,30 @@ import sys
 
 from config import SKIPCOLS
 
-aggmap = namedtuple('aggmap', ('newcol', 'oldcols'))
-qinfo = namedtuple('qinfo', ('ix', 'len'))
+Aggmap = namedtuple('Aggmap', ('newcol', 'oldcols'))
+Qinfo = namedtuple('Qinfo', ('ix', 'len'))
 AGGLIST = [
-    (10, [aggmap('Not very', ('- very unsatisfied', '- 2', '- 3', '- 4', '- 5')),
-          aggmap('Quite', ('- satisfied', '- 7', '- 8')),
-          aggmap('Very', ('-9', '- extremely satisfied'))
+    (10, [Aggmap('Not very', ('- very unsatisfied', '- 2', '- 3', '- 4',
+                              '- 5')),
+          Aggmap('Quite', ('- satisfied', '- 7', '- 8')),
+          Aggmap('Very', ('- 9', '- extremely satisfied'))
           ]),
-    (13, [aggmap('Not very', ('Very unlikely', 'Unlikely', 'Neither likely nor unlikely')),
-          aggmap('Likely', ('Likely', 'Very likely'))
+    (13, [Aggmap('Not very', ('Very unlikely', 'Unlikely',
+                              'Neither likely nor unlikely')),
+          Aggmap('Likely', ('Likely', 'Very likely'))
           ]),
-    (15, [aggmap('Female', ('Female',)),
-          aggmap('Male', ('Male',))
+    (15, [Aggmap('Female', ('Female',)),
+          Aggmap('Male', ('Male',))
           ]),
-    (16, [aggmap('Under 55', ('Under 16', '16 - 34', '35 - 54')),
-          aggmap('55 or over', ('55 - 64', '65 or over'))
+    (16, [Aggmap('Under 55', ('Under 16', '16 - 34', '35 - 54')),
+          Aggmap('55 or over', ('55 - 64', '65 or over'))
           ]),
-    (17, [aggmap('Not disabled', ('No',)),
-          aggmap('Disabled', ('Yes, limited a little', 'Yes, limited a lot')),
+    (17, [Aggmap('Not disabled', ('No',)),
+          Aggmap('Disabled', ('Yes, limited a little', 'Yes, limited a lot')),
           ]),
-    (18, [aggmap('White British',
+    (18, [Aggmap('White British',
                  ('White - British (English/Scottish/Welsh/Northern Irish)',)),
-          aggmap('Other', ('White - Irish', 'other White', 'Indian',
+          Aggmap('Other', ('White - Irish', 'other White', 'Indian',
                            'Pakistani', 'Bangladeshi', 'Chinese',
                            'other Asian background', 'Black African',
                            'Black Caribbean', 'other Black background',
@@ -57,22 +60,13 @@ AGGLIST = [
                            'Arab', 'Mixed / Multiple ethnic group(s)',
                            'other ethnicity'))
           ]),
-    (19, [aggmap('Harrow', ('Harrow borough',)),
-          aggmap('Other London', ('other London borough',)),
-          aggmap('Elsewhere', ('elsewhere in UK', 'Not in UK')),
+    (19, [Aggmap('Harrow', ('Harrow borough',)),
+          Aggmap('Other London', ('other London borough',)),
+          Aggmap('Elsewhere', ('elsewhere in UK', 'Not in UK')),
           ]),
 ]
-'''
-AGGLIST = [
-    (10, [aggmap('Not very', ('- very unsatisfied', '-2', '-3', '-4', '-5')),
-          aggmap('Quite', ('- satisfied', '-7', '-8')),
-          aggmap('Very', ('-9', '- extremely satisfied'))
-          ]),
-]
-'''
+
 AGGDICT = OrderedDict(AGGLIST)
-INV_AGGDICT = None  # this will be populated in the main code below
-# print(AGGDICT)
 
 
 def trace(level, template, *arglist):
@@ -84,11 +78,11 @@ def get_question_dict(qrow):
     """
     :param qrow: The list of the first row returned from the CSV file which has
                  "q1", "q2", ... in question columns and "" otherwise.
-    :return: OrderedDict mapping question number to a qinfo namedtuple of
+    :return: OrderedDict mapping question number to a Qinfo namedtuple of
             (offset, length) where length is the number of answers to each
             question
     """
-    # qrow[i][1:] maps q21 -> 21
+    # qrow[i][1:] maps 'q21' -> '21'
     # qnlist is [(1, index1), (2, index2), ...]
     qnlist = [(int(qrow[i][1:]), i) for i in range(SKIPCOLS, len(qrow))
               if qrow[i]]
@@ -99,8 +93,8 @@ def get_question_dict(qrow):
     qndict[lastq + 1] = len(qrow)  # insert dummy question at end
     trace(2, 'qndict: {}', qndict)
     lir = list(enumerate(qndict.items()))
-    # lir = [(0, (1, IX1)), (1, (2, IX2)), ... , (minor, (minor+1, IXn)]
-    # where IXn is the minor-th question's index in the row
+    # lir = [(0, (1, IX1)), (1, (2, IX2)), ... , (n, (n+1, IXn)]
+    # where IXn is the n-th question's index in the row
     # print(lir)
     qdict = OrderedDict()
     for i, qix in lir[:-1]:  # iterate over all but the dummy last entry
@@ -112,7 +106,7 @@ def get_question_dict(qrow):
         # lir[i][1][1] = ix
         # print(f'len = {lir[i + 1][1][1] - qix[1]}')
         anslen = lir[i + 1][1][1] - qix[1]
-        qdict[qix[0]] = qinfo(ix=qix[1], len=anslen)
+        qdict[qix[0]] = Qinfo(ix=qix[1], len=anslen)
     return qdict
 
 
@@ -120,12 +114,27 @@ def inv_aggdict():
     """
     Convert AGGDICT to a dictionary of dictionaries where for each question the
     keys are the original column names and the values are the aggregate column
-    names.
+    names. For example, assuming we have only question 16 where AGGLIST has:
+
+        (16, [Aggmap('Under 55', ('Under 16', '16 - 34', '35 - 54')),
+              Aggmap('55 or over', ('55 - 64', '65 or over'))
+              ]),
+
+    INV_AGGDICT will be:
+
+    {16: {
+            'Under 16': 'Under 55',
+            '16 - 34': 'Under 55',
+            '35 - 54': 'Under 55',
+            '55 - 64': '55 or over',
+            '65 or over': '55 or over'
+        }
+    }
     :return: The inverted dictionary
     """
     invdict = OrderedDict()
     for q in AGGDICT:
-        anslist = AGGDICT[q]  # the list of aggmap namedtuples for this question
+        anslist = AGGDICT[q]  # the list of Aggmap namedtuples for this question
         invmap = OrderedDict()
         for amap in anslist:
             for origcol in amap.oldcols:
@@ -135,7 +144,7 @@ def inv_aggdict():
     return invdict
 
 
-def new_q_row(qrow, qdict):
+def get_question_row(qrow, qdict):
     """
     This is used for rows 1 and 2.
     For each question, insert the question text followed by a number of empty
@@ -163,33 +172,33 @@ def new_q_row(qrow, qdict):
     return nqr
 
 
-def new_ans_map(arow, qdict):
+def new_ans_map(ansrow, qdict):
     """
     Process row #3 in the CSV file containing answer titles. Create a dict
     mapping original answer columns to aggregated answer columns. Later, when
     processing the data rows, we will copy non-empty original columns to the
     corresponding aggregated columns.
-    :param arow: Third row of the CSV file
+    :param ansrow: Third row of the CSV file
     :param qdict: The OrderedDict created by get_question_dict
     :return: dict for ea column, the corresponding aggregated answer if one
              exists.
     """
     amap = {}
-    newarow = [arow[i] for i in range(SKIPCOLS)]  # initialize to constant cols
+    newarow = [ansrow[i] for i in range(SKIPCOLS)]  # initialize to constants
     for question, qinf in qdict.items():
         ix = qinf.ix
         length = qinf.len
         if question not in INV_AGGDICT:
             # just copy the original questions
-            newarow += arow[ix:ix + length]
+            newarow += ansrow[ix:ix + length]
             continue
         oamap = INV_AGGDICT[question]  # orig ans -> agg ans
         agdict = AGGDICT[question]  # agg ans -> orig answers
-        for qi in agdict:  # qi is a qinfo namedtuple
-            newarow.append(qi.newcol)
-        # Build a list of column offset -> agg column name
+        for am in agdict:  # qi is an Aggmap namedtuple
+            newarow.append(am.newcol)
+        # Build a dict of original column offset -> agg column name
         for coln in range(ix, ix + length):
-            old_ans = arow[coln].strip()
+            old_ans = ansrow[coln].strip()
             try:
                 amap[coln] = oamap[old_ans]
             except KeyError:
@@ -204,15 +213,15 @@ def new_data_row(row, qdict, namap):
     """
 
     :param row: the next data row
-    :param qdict: created by new_q_row
+    :param qdict: created by get_question_row
     :param namap: created by new_ans_map
-    :return:
+    :return: the newly constructed row with aggregated answers
     """
     newrow = [row[i] for i in range(SKIPCOLS)]  # initialize to constant cols
     for question, qinf in qdict.items():
         ix = qinf.ix
         length = qinf.len
-        if question not in INV_AGGDICT:
+        if question not in AGGDICT:
             newrow += row[ix:ix + length]
             continue
         aggd = AGGDICT[question]
@@ -232,29 +241,85 @@ def new_data_row(row, qdict, namap):
     return newrow
 
 
+def check_1_answer(qnum, agmlist, row3):
+    """
+
+    :param qnum: the question number
+    :param agmlist: the list of Aggmaps for this question
+    :param row3: the slice of row3 for this question
+    :return: None
+    """
+    # build a set of answers in the config
+    orig_answers = set()
+    for aggmap in agmlist:
+        for ans in aggmap.oldcols:
+            if ans in orig_answers:
+                trace(1, 'Q{}: duplicate answer: {}', qnum, ans)
+            else:
+                orig_answers.add(ans)
+    trace(2, 'cfg orig: {}', orig_answers)
+    # Now we have a set of the original answers from the config and a list of
+    # the answer names from the CSV file for this question in row3.
+    #
+    for cell in [r.strip() for r in row3]:
+        if cell in orig_answers:
+            orig_answers.remove(cell)
+        else:
+            # An ignored answer is one that is in the CSV file but is not
+            # aggregated. This may be intended.
+            trace(1, '    Q{}: ignored answer: "{}"', qnum, cell)
+    #
+    # Check for leftover answers in the config file
+    for answer in orig_answers:
+        trace(1, '    Q{}: unused answer: "{}"', qnum, answer)
+
+
+def check_answers(question_dict, answer_text_row):
+    """
+    Display diagnostics for inconsistencies between the configuration and the
+    data in the spreadsheet.
+
+    :param question_dict: question # -> (column index, length)
+    :param answer_text_row: row 3 of the CSV file
+    :return:
+    """
+    for qn, agmlist in AGGDICT.items():  # agmlist is a list of Aggmap items.
+        qinfo = question_dict[qn]
+        ix = qinfo.ix
+        length = qinfo.len
+        trace(1, 'Checking answer {}, ix = {}, length = {}.', qn, ix, length)
+        check_1_answer(qn, agmlist, answer_text_row[ix:ix + length])
+
+
 def main():
     reader = csv.reader(infile)
     writer = csv.writer(outfile)
+
     # row 1: has values like q1,,,,q2,,,q3,,etc.
     q_row = next(reader)
     trace(2, 'q_row(len {}): {}', len(q_row), q_row)
-    q_dict = get_question_dict(q_row)
-    # q_dict: question # -> (column index, length)
-    nq_row = new_q_row(q_row, q_dict)
+    # question_dict: question # -> (column index, length)
+    question_dict = get_question_dict(q_row)
+    nq_row = get_question_row(q_row, question_dict)
     trace(2, 'nq_row(len {}): {}', len(nq_row), nq_row)
     writer.writerow(nq_row)
+
     # row 2: contains question text in cols under question #'s
     question_text_row = next(reader)
-    nq_row2 = new_q_row(question_text_row, q_dict)
+    nq_row2 = get_question_row(question_text_row, question_dict)
     trace(2, 'nq_row2(len {}): {}', len(nq_row2), nq_row2)
     writer.writerow(nq_row2)
+
     # row 3: contains question answers
     answer_text_row = next(reader)
-    na_map, na_row = new_ans_map(answer_text_row, q_dict)
-    writer.writerow(na_row)
-    # rows 4-minor:
+    if args.check:
+        check_answers(question_dict, answer_text_row)
+    na_map, na_row = new_ans_map(answer_text_row, question_dict)
+    writer.writerow(na_row)  # write row 3 with the aggregated answers
+
+    # rows 4-n: the survey records
     for row in reader:
-        newrow = new_data_row(row, q_dict, na_map)
+        newrow = new_data_row(row, question_dict, na_map)
         writer.writerow(newrow)
 
 
@@ -262,6 +327,10 @@ def getargs():
     parser = argparse.ArgumentParser(
         description='''Copy a 'cleaned' CSV file, aggregating answers to
                     questions as specified by an internal configuration.
+        ''')
+    parser.add_argument('-c', '--check', action='store_true',
+                        help='''Check that the aggregation configuration
+                        matches the CSV file.
         ''')
     parser.add_argument('infile', help='''
          The CSV file that has been cleaned by extract_csv.sh''')
@@ -277,8 +346,6 @@ def getargs():
 if __name__ == '__main__':
     if sys.version_info.major < 3 or sys.version_info.minor < 6:
         raise ImportError('requires Python 3.6')
-    # invd = inv_aggdict()
-    # print(invd)
     args = getargs()
     infile = codecs.open(args.infile, 'r', 'utf-8-sig')
     outfile = codecs.open(args.outfile, 'w', 'utf-8-sig')
